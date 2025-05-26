36494c53efcfd7390ac0a9ca75cc612b5b622e50
Heliosstudio <studio.helios10@gmail.com>
2025-05-26 22:06:35 +0300

    player2 added

--- Assets/Scripts/Player2Controller.cs
+++ Assets/Scripts/Player2Controller.cs
@@ -1,6 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player2Controller
+public class Player2Controller : MonoBehaviour
 {
-    
+    public float moveSpeed = 5f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    private Vector2 moveInput;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(moveInput.x, moveInput.y, 0f);
+        transform.Translate(move * moveSpeed * Time.deltaTime);
+    }
 }
