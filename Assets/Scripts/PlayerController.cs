625978c51a9c84fabd31b1f7eaa81e334145148b
Heliosstudio <studio.helios10@gmail.com>
2025-05-26 17:07:25 +0300

    Player and bullet controller added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    private Vector2 moveInput;
+
+    void Update()
+    {
+        // Hareket
+        transform.Translate(moveInput * moveSpeed * Time.deltaTime);
+    }
+
+    // Input System üzerinden gelen yön verisi
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    // Ateþ
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        }
+    }
+}
